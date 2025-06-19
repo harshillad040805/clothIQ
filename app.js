80ed40abf94f1421afa763f5ebe8fcad47e6fcf0
harshillad040805 <harshillad4805@gmail.com>
2025-06-19 18:43:39 +0530

    port changed

--- app.js
+++ app.js
@@ -159,7 +159,7 @@ app.get('/virtual-try-on', (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
